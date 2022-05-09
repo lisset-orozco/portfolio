ce8470a82292a3e77bcf7385c9e198ef5fd83b5b
lisset-orozco <liss612@gmail.com>
2022-05-09 15:30:33 -0500

    [ADD] Stocks::FindMarketPrice use case
     - add a use case that contains the steps to request the price in the market
     - update V1::StocksController in action market_price with the use case
     - add Stocks::MarketPriceService to request the price in the market

--- app/controllers/v1/stocks_controller.rb
+++ app/controllers/v1/stocks_controller.rb
@@ -24,6 +24,12 @@ class V1::StocksController < ApplicationController
   end
 
   def market_price
-    render(json: { test: 'market_price' })
+    result = Stocks::FindMarketPriceUseCase.call(params)
+
+    if result.success?
+      render json: { market_price: result.payload }
+    else
+      render json: { error: result.error }, status: :unprocessable_entity
+    end
   end
 end
---
+++ app/services/stocks/market_price_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Stocks
+  class MarketPriceService < ApplicationService
+    def initialize(params)
+      super()
+      @params = params
+    end
+
+    def call
+      response = HttpClientUtil.call('get', build_params) 
+      result = handle_response(response)
+
+      response(success: true, payload: result )
+    rescue StandardError
+      response(error: StandardError.new(self))
+    end
+
+    private
+
+    attr_reader :params
+
+    def build_params
+      url = 'https://api.binance.com/api/v3/klines'
+      uri = URI(url)
+      uri.query = URI.encode_www_form(query_params)
+      { url: uri }
+    end
+
+    def handle_response(response)
+      raise unless (200..299).include?(response.code)
+
+      result = JSON.parse(response.body)
+      result.present? ? result[0][4] : ''
+    end
+
+    def query_params
+      miliseconds = (params[:date].to_time.to_f * 1000).to_i
+      {
+        symbol: params[:symbol],
+        interval: '1d',
+        startTime: miliseconds,
+        endTime: miliseconds
+      }
+    end
+  end
+end
