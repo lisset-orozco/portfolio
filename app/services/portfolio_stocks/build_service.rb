187c9da021552f2156841871b0c417aac64f2bec
lisset-orozco <liss612@gmail.com>
2022-05-08 22:21:41 -0500

    [ADD] PortfolioStocks::BuildService file

---
+++ app/services/portfolio_stocks/build_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module PortfolioStocks
+  class BuildService < ApplicationService
+    def initialize(params)
+      super()
+      @params = params
+    end
+
+    def call
+      portfolio_stock = PortfolioStock.new(
+        portfolio: params[:portfolio],
+        stock_id: params[:stock_id]
+      )
+      response(success: true, payload: portfolio_stock)
+    rescue StandardError
+      response(error: StandardError.new(self))
+    end
+
+    private
+
+    attr_reader :params
+  end
+end
