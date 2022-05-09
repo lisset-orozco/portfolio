be3538bdea776fa49428669ffb230b75d8a39704
lisset-orozco <liss612@gmail.com>
2022-05-08 22:16:55 -0500

    [ADD] Stocks::FindOrCreateService and ApplicationService files

---
+++ app/services/application_service.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ApplicationService
+  def self.call(*args, &block)
+    new(*args, &block).call
+  end
+
+  private
+
+  def response(success: false, payload: {}, error: nil)
+    OpenStruct.new(success?: success, payload:, error:)
+  end
+end
---
+++ app/services/stocks/find_or_create_service.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Stocks
+  class FindOrCreateService < ApplicationService
+    def initialize(params)
+      super()
+      @params = params
+    end
+
+    def call
+      response(success: true, payload: find_or_create(params[:symbol]))
+    rescue StandardError
+      response(error: StandardError.new(self))
+    end
+
+    private
+
+    attr_reader :params
+
+    def find_or_create(symbol)
+      Stock.transaction(requires_new: true) do
+        Stock.find_or_create_by(symbol: symbol)
+      end
+    rescue ActiveRecord::RecordNotUnique
+      retry
+    end
+  end
+end
