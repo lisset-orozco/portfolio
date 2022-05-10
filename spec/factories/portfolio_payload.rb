d7c22d03a38f766e4503cd293f42290fb5cf8360
lisset-orozco <liss612@gmail.com>
2022-05-09 23:35:04 -0500

    [ADD] Portfolio request specs
     - add portfolio_payload factory
     - add vcr for profit action
     - add requests specs

---
+++ spec/factories/portfolio_payload.rb
@@ -0,0 +1,43 @@
+FactoryBot.define do
+  factory :portfolio_payload, class: Hash do
+    initialize_with do
+      {
+        'name': 'personal',
+        'description': 'finanza personal',
+        'stocks': {
+          'ROSEUSDT': [
+            {
+              'amount':  1.5,
+              'purchase_date': '2022-04-29',
+              'price': 36.5
+            },
+            {
+              'amount':  5,
+              'purchase_date': '2022-04-30',
+              'price': 34.3
+            }
+          ],
+          'SLPUSDT': [
+            {
+              'amount':  600,
+              'purchase_date': '2022-04-29',
+              'price': 0.09
+            },
+            {
+              'amount':  300,
+              'purchase_date': '2022-04-30',
+              'price': 0.08
+            }
+          ],
+          'ETHUSDT': [
+            {
+              'amount':  1,
+              'purchase_date': '2022-04-29',
+              'price': 2549
+            }
+          ]
+        }
+      }
+    end
+  end
+end
